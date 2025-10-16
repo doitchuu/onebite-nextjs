39e878af1855fbdb53193af26b886e2044359bf0
doitchuu <seulgichu14@gmail.com>
2025-10-17 00:41:34 +0900

    feat: 라우터 객체 네비게이팅 구현

--- onebite-books-page-router/src/pages/_app.tsx
+++ onebite-books-page-router/src/pages/_app.tsx
@@ -1,7 +1,10 @@
 import type { AppProps } from "next/app";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
   return (
     <>
       <header>
@@ -9,7 +12,13 @@ export default function App({ Component, pageProps }: AppProps) {
         &nbsp;
         <Link href="/search">검색</Link>
         &nbsp;
-        <Link href="/book/1">1번 도서</Link>
+        <button
+          onClick={() => {
+            router.push("/book/1");
+          }}
+        >
+          1번 도서
+        </button>
       </header>
       <Component {...pageProps} />
     </>
