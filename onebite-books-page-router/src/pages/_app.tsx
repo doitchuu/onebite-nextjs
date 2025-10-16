a0306453eab965950eb686851fe7f4628a436672
doitchuu <seulgichu14@gmail.com>
2025-10-17 00:34:51 +0900

    feat: Link 컴포넌트 네비게이팅 구현

--- onebite-books-page-router/src/pages/_app.tsx
+++ onebite-books-page-router/src/pages/_app.tsx
@@ -1,9 +1,16 @@
 import type { AppProps } from "next/app";
+import Link from "next/link";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <header>Header</header>
+      <header>
+        <Link href="/">홈</Link>
+        &nbsp;
+        <Link href="/search">검색</Link>
+        &nbsp;
+        <Link href="/book/1">1번 도서</Link>
+      </header>
       <Component {...pageProps} />
     </>
   );
