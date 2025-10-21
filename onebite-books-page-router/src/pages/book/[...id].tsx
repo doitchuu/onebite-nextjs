0872625af86eb6e9f1101fddffa2be22d6edb89f
doitchuu <seulgichu14@gmail.com>
2025-10-21 22:38:11 +0900

    feat: 홈페이지 UI 개선 및 도서 상세 페이지 추가
    - 홈페이지에 도서 목록 표시 기능 추가
    - 추천 도서 및 전체 도서 섹션 구성
    - 도서 상세 페이지 UI 구현
    - CSS 모듈을 활용한 스타일링
    - getLayout 패턴 적용으로 레이아웃 시스템 구축
    - 전역 스타일 개선

--- onebite-books-page-router/src/pages/book/[...id].tsx
+++ onebite-books-page-router/src/pages/book/[...id].tsx
@@ -1,13 +1,24 @@
 import { useRouter } from "next/router";
+import style from "./[id].module.css";
+import books from "@/mock/books.json";
 
 export default function Page() {
+  const { id, title, subTitle, description, author, publisher, coverImgUrl } =
+    books[0];
   const router = useRouter();
 
-  console.log(router, "router");
-
   return (
-    <div>
-      <h1>{router.query.id}도서 상세 페이지</h1>
+    <div className={style.container}>
+      <div className={style.cover_img_container}>
+        <img src={coverImgUrl} alt="커버 이미지" />
+      </div>
+      <div className={style.title}>{title}</div>
+      <div className={style.subTitle}>{subTitle}</div>
+      <br />
+      <div className={style.author}>
+        {author} | {publisher}
+      </div>
+      <div className={style.description}>{description}</div>
     </div>
   );
 }
