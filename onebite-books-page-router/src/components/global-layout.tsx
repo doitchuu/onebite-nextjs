1b0f83b52305361d38a890d3c9d19d9ee28dfd07
doitchuu <seulgichu14@gmail.com>
2025-10-21 22:38:07 +0900

    feat: 컴포넌트 및 레이아웃 시스템 추가
    - GlobalLayout 컴포넌트 추가
    - SearchbarLayout 컴포넌트 추가
    - BookItem 컴포넌트 추가
    - 각 컴포넌트별 CSS 모듈 스타일 추가
    - Book 타입 정의 추가
    - Mock 데이터 추가 (books.json)

--- onebite-books-page-router/src/components/global-layout.tsx
+++ onebite-books-page-router/src/components/global-layout.tsx
@@ -1,14 +1,15 @@
 import Link from "next/link";
 import { ReactNode } from "react";
+import style from "./global-layout.module.css";
 
 export default function GlobalLayout({ children }: { children: ReactNode }) {
   return (
-    <>
+    <div className={style.container}>
       <header>
         <Link href="/">BOOKS</Link>
       </header>
       <main>{children}</main>
       <footer>@doitchuu</footer>
-    </>
+    </div>
   );
 }
--- onebite-books-page-router/src/components/searchbar-layout.tsx
+++ onebite-books-page-router/src/components/searchbar-layout.tsx
@@ -1,10 +1,47 @@
-import { ReactNode } from "react";
+import { useRouter } from "next/router";
+import { ReactNode, useEffect, useState } from "react";
+import style from "./searchbar-layout.module.css";
 
 export default function SearchbarLayout({ children }: { children: ReactNode }) {
+  const router = useRouter();
+  const [search, setSearch] = useState("");
+
+  const onSubmit = () => {
+    if (!search || router.query.q === search) {
+      return;
+    }
+
+    router.push(`/search?q=${search}`);
+  };
+
+  const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      onSubmit();
+    }
+  };
+
+  useEffect(() => {
+    setSearch((router.query.q as string) || "");
+  }, [router.query.q]);
+
   return (
     <>
-      <div>임시 검색 폼</div>
-      {children}
+      <div>
+        <div className={style.searchbar_container}>
+          <input
+            value={search}
+            onChange={onChangeSearch}
+            onKeyDown={onKeyDown}
+            placeholder="도서 검색"
+          />
+          <button onClick={onSubmit}>검색</button>
+        </div>
+        {children}
+      </div>
     </>
   );
 }
