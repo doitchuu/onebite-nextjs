aa3757ca95472186aa445c02b2a535259c882f1b
doitchuu <seulgichu14@gmail.com>
2025-11-10 03:56:03 +0900

    feat: 검색 페이지 API 연동 및 검색 결과 표시 기능 구현

--- onebite-books-page-router/src/pages/search/index.tsx
+++ onebite-books-page-router/src/pages/search/index.tsx
@@ -1,13 +1,29 @@
 import SearchbarLayout from "@/components/searchbar-layout";
 import { useRouter } from "next/router";
 import { ReactNode } from "react";
+import BookItem from "@/components/book-item";
 
-export default function Page() {
+import fetchBooks from "@/lib/fetch-books";
+
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const q = context.query.q;
+  const books = await fetchBooks(q as string);
+  return { props: { books } };
+}
+
+export default function Page({
+  books,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const router = useRouter();
 
   return (
     <div>
       <h1>검색: {router.query.q}</h1>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
     </div>
   );
 }
