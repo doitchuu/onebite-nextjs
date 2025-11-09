00d2afbf2f998ee3721ff17a1007964126bafa7d
doitchuu <seulgichu14@gmail.com>
2025-11-10 03:56:04 +0900

    feat: 도서 상세 페이지 API 연동 및 getServerSideProps 적용

--- onebite-books-page-router/src/pages/book/[...id].tsx
+++ onebite-books-page-router/src/pages/book/[...id].tsx
@@ -1,14 +1,27 @@
-import { useRouter } from "next/router";
 import style from "./[id].module.css";
-import books from "@/mock/books.json";
 
-export default function Page() {
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import fetchOneBook from "@/lib/fetch-one-book";
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const id = context.params!.id;
+  const book = await fetchOneBook(Number(id));
+
+  return { props: { book } };
+}
+
+export default function Page({
+  book,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  if (!book) {
+    return <div>오류가 발생했어요. 다시 시도해 주세요.</div>;
+  }
+
   const { id, title, subTitle, description, author, publisher, coverImgUrl } =
-    books[0];
-  const router = useRouter();
+    book;
 
   return (
-    <div className={style.container}>
+    <div className={style.container} key={id}>
       <div className={style.cover_img_container}>
         <img src={coverImgUrl} alt="커버 이미지" />
       </div>
