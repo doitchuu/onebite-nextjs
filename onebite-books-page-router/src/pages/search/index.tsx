7fe34c3c34f5ef2ac9ae71d98bb418ad0962a838
doitchuu <seulgichu14@gmail.com>
2025-11-29 01:36:12 +0900

    feat: search 페이지에 SEO 메타 태그 추가

--- onebite-books-page-router/src/pages/search/index.tsx
+++ onebite-books-page-router/src/pages/search/index.tsx
@@ -7,6 +7,7 @@ import fetchBooks from "@/lib/fetch-books";
 
 import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
 import { BookData } from "@/types";
+import Head from "next/head";
 
 export default function Page() {
   const [books, setBooks] = useState<BookData[]>([]);
@@ -26,12 +27,23 @@ export default function Page() {
   }, [q]);
 
   return (
-    <div>
-      <h1>검색: {router.query.q}</h1>
-      {books.map((book) => (
-        <BookItem key={book.id} {...book} />
-      ))}
-    </div>
+    <>
+      <Head>
+        <title>한입북스 - 검색결과</title>
+        <meta property="og:title" content="한입북스-검색결과" />
+        <meta
+          property="og:description"
+          content="한입북스에 등록된 도서들을 만나보세요."
+        />
+        <meta property="og:image" content="/thumbnail.png" />
+      </Head>
+      <div>
+        <h1>검색: {router.query.q}</h1>
+        {books.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
+      </div>
+    </>
   );
 }
 
